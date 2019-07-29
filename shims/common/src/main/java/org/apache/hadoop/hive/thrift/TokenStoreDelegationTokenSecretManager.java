56b795d01f4e47be8cca08e0c87cd8f7b1386f55
Jon Alberdi <j.alberdi@criteo.com>
2019-07-29 10:42:25 +0200

    Revert "Support delegation token renewal"
    This reverts commit 4f3bdaa8fb3b9f070ffdb9687cf71c8a074d8e58.

--- shims/common/src/main/java/org/apache/hadoop/hive/thrift/TokenStoreDelegationTokenSecretManager.java
+++ shims/common/src/main/java/org/apache/hadoop/hive/thrift/TokenStoreDelegationTokenSecretManager.java
@@ -172,10 +172,7 @@ public class TokenStoreDelegationTokenSecretManager extends DelegationTokenSecre
     synchronized (this) {
       super.currentTokens.put(id,  tokenInfo);
       try {
-        long res = super.renewToken(token, renewer);
-        this.tokenStore.removeToken(id);
-        this.tokenStore.addToken(id, super.currentTokens.get(id));
-        return res;
+        return super.renewToken(token, renewer);
       } finally {
         super.currentTokens.remove(id);
       }
